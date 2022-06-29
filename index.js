5c889fe0ae64e3287551a236fe33cd00058b2dc4
crash <charlychaplin83@gmail.com>
2022-06-29 13:06:38 +0700

    two

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+
+
 app.get('/', (req, res) => {
 	res.send("Hello, client.")
 })
 
 
-
+app.post('/auth/login', (req, res) => {
+	console.log(req.body)
+	res.json({
+		success: true,
+	})
+})
 
 app.listen(4444, err => {
 	return err ? console.log(err) : console.log("Everything is OK")
