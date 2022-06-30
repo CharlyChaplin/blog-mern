4b3223dc7a90afc38353ec1650f831885a0c0e49
CharlyChaplin <charlychaplin83@gmail.com>
2022-06-30 08:00:12 +0700

    Three

--- index.js
+++ index.js
@@ -1,21 +1,103 @@
 import express from "express";
+import { validationResult } from "express-validator";
+import jsonwebtoken from "jsonwebtoken";
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt'
+import { registerValidation } from "./validations/auth.js";
+import UserModel from './models/User.js';
+
+mongoose
+	.connect('mongodb+srv://admin:wwwwww@cluster0.dezs5xl.mongodb.net/blog?retryWrites=true&w=majority')
+	.then(() => console.log('DB OK'))
+	.catch((err) => console.log('DB Error!: ', err))
 
 const app = express();
+const jwt = jsonwebtoken;
 
 app.use(express.json());
 
+app.post('/auth/login', async (req, res) => {
+	try {
+		const user = await UserModel.findOne({ email: req.body.email })
+		if (!user) {
+			return res.status(404).json({
+				message: 'User not found',
+			})
+		}
 
+		const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+		if (!isValidPass) {
+			return res.status(400).json({
+				message: "Wrong password"
+			})
+		};
 
-app.get('/', (req, res) => {
-	res.send("Hello, client.")
+		const token = jwt.sign({
+			_id: user._id,
+		},
+			'secret123',
+			{
+				expiresIn: '30d'
+			}
+		);
+		const { passwordHash, ...userData } = user._doc;
+
+		res.json({
+			...userData,
+			token
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: 'auth was fault'
+		})
+	}
 })
 
+app.post('/auth/register', registerValidation, async (req, res) => {
+	try {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(400).json(errors.array());
+		}
 
-app.post('/auth/login', (req, res) => {
-	console.log(req.body)
-	res.json({
-		success: true,
-	})
+		const password = req.body.password;
+		const salt = await bcrypt.genSalt(10);
+		const hash = await bcrypt.hash(password, salt);
+
+		const doc = new UserModel({
+			email: req.body.email,
+			fullName: req.body.fullName,
+			avatarUrl: req.body.avatarUrl,
+			passwordHash: hash,
+		})
+
+		const user = await doc.save();
+
+		const token = jwt.sign({
+			_id: user._id,
+		},
+			'secret123',
+			{
+				expiresIn: '30d'
+			}
+		)
+
+		const { passwordHash, ...userData } = user._doc;
+
+		res.json({
+			...userData,
+			token
+		});
+		res.json({
+			success: true,
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: 'register was fault'
+		})
+	}
 })
 
 app.listen(4444, err => {
---
+++ validations/auth.js
@@ -0,0 +1,9 @@
+import { body } from "express-validator";
+
+
+export const registerValidation = [
+	body('email', 'Wrong e-mail format').isEmail(),
+	body('password', "The password must be 5 symbols at least").isLength({min: 5}),
+	body('fullName', "Enter a name").isLength({min: 3}),
+	body('avatarUrl', "Wrong the link for the avatar").optional().isURL(),
+]
