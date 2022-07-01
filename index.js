453163ab3b4b5bdcd865ba38069837a1dcb6b22e
crash <charlychaplin83@gmail.com>
2022-07-01 16:23:36 +0700

    Four

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import express from "express";
 import { validationResult } from "express-validator";
-import jsonwebtoken from "jsonwebtoken";
 import mongoose from "mongoose";
-import bcrypt from 'bcrypt'
 import { registerValidation } from "./validations/auth.js";
-import UserModel from './models/User.js';
+import checkAuth from "./utils/checkAuth.js";
+import * as UserController from "./controllers/UserController.js";
 
 mongoose
 	.connect('mongodb+srv://admin:wwwwww@cluster0.dezs5xl.mongodb.net/blog?retryWrites=true&w=majority')
@@ -12,93 +11,15 @@ mongoose
 	.catch((err) => console.log('DB Error!: ', err))
 
 const app = express();
-const jwt = jsonwebtoken;
 
 app.use(express.json());
 
-app.post('/auth/login', async (req, res) => {
-	try {
-		const user = await UserModel.findOne({ email: req.body.email })
-		if (!user) {
-			return res.status(404).json({
-				message: 'User not found',
-			})
-		}
+app.post('/auth/login', UserController.login)
 
-		const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
-		if (!isValidPass) {
-			return res.status(400).json({
-				message: "Wrong password"
-			})
-		};
+app.post('/auth/register', registerValidation, UserController.register)
 
-		const token = jwt.sign({
-			_id: user._id,
-		},
-			'secret123',
-			{
-				expiresIn: '30d'
-			}
-		);
-		const { passwordHash, ...userData } = user._doc;
+app.get('/auth/me', checkAuth, UserController.getMe)
 
-		res.json({
-			...userData,
-			token
-		});
-	} catch (err) {
-		console.log(err);
-		res.status(500).json({
-			message: 'auth was fault'
-		})
-	}
-})
-
-app.post('/auth/register', registerValidation, async (req, res) => {
-	try {
-		const errors = validationResult(req)
-		if (!errors.isEmpty()) {
-			return res.status(400).json(errors.array());
-		}
-
-		const password = req.body.password;
-		const salt = await bcrypt.genSalt(10);
-		const hash = await bcrypt.hash(password, salt);
-
-		const doc = new UserModel({
-			email: req.body.email,
-			fullName: req.body.fullName,
-			avatarUrl: req.body.avatarUrl,
-			passwordHash: hash,
-		})
-
-		const user = await doc.save();
-
-		const token = jwt.sign({
-			_id: user._id,
-		},
-			'secret123',
-			{
-				expiresIn: '30d'
-			}
-		)
-
-		const { passwordHash, ...userData } = user._doc;
-
-		res.json({
-			...userData,
-			token
-		});
-		res.json({
-			success: true,
-		});
-	} catch (err) {
-		console.log(err);
-		res.status(500).json({
-			message: 'register was fault'
-		})
-	}
-})
 
 app.listen(4444, err => {
 	return err ? console.log(err) : console.log("Everything is OK")
--- validations/auth.js
+++ validations/auth.js
@@ -1,6 +1,5 @@
 import { body } from "express-validator";
 
-
 export const registerValidation = [
 	body('email', 'Wrong e-mail format').isEmail(),
 	body('password', "The password must be 5 symbols at least").isLength({min: 5}),
