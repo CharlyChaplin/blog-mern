c8dcf2617f1e930903f266daa9a6cd69199f1e58
crash <charlychaplin83@gmail.com>
2022-07-04 16:54:28 +0700

    Six

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,10 +1,86 @@
+import { json } from 'express';
 import PostModel from '../models/Post.js'
 
 export const getAll = async (req, res) => {
 	try {
 		const posts = await PostModel.find().populate('user').exec();
-		
+
 		res.json(posts)
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({
+			message: "Fault to get all posts."
+		})
+	}
+}
+
+export const getOne = async (req, res) => {
+	try {
+		const postId = req.params.id;
+
+		//увеличиваем кол-во просмотров через обновление статьи
+		PostModel.findOneAndUpdate(
+			{
+				_id: postId,
+			},
+			//что мы хотим обновить и на сколько
+			{
+				$inc: { viewsCount: 1 },
+			},
+			//не просто обновить, но обновить и вернуть результат
+			{
+				returnDocument: 'after',
+			},
+			//была ошибка или же пришёл документ
+			(err, doc) => {
+				console.log(doc)
+				if (err) {
+					console.log(err)
+					return res.status(500).json({
+						message: "Fault to get one post."
+					})
+				}
+				if (!doc) {
+					return res.status(404).json({
+						message: "The post not found."
+					})
+				}
+
+				res.json(doc);
+			}
+		)
+	} catch (err) {
+		console.log(err)
+		res.status(500).json({
+			message: "Fault to get all posts."
+		})
+	}
+}
+
+export const remove = async (req, res) => {
+	try {
+		const postId = req.params.id;
+
+		PostModel.findOneAndDelete({
+			_id: postId
+		}, (err, doc) => {
+			if (err) {
+				console.log(err);
+				return res.status(404).json({
+					message: "Fault to delete the post."
+				})
+			}
+			if (!doc) {
+				return res.status(404).json({
+					message: "The post not found for delete."
+				})
+			}
+
+			res.json({
+				success: true
+			})
+		})
+
 	} catch (err) {
 		console.log(err)
 		res.status(500).json({
@@ -22,14 +98,41 @@ export const create = async (req, res) => {
 			tags: req.body.tags,
 			user: req.userId,
 		});
-		
+
 		const post = await doc.save();
 		res.json(post);
-		
+
 	} catch (err) {
 		console.log(err)
 		res.status(500).json({
 			message: "Fault to creating the post."
+		})
+	}
+}
+
+export const update = async (req, res) => {
+	try {
+		const postId = req.params.id;
+		await PostModel.updateOne(
+			{
+				_id: postId
+			},
+			//что мы хотим обновить
+			{
+				title: req.body.title,
+				text: req.body.text,
+				imageUrl: req.body.imageUrl,
+				user: req.userId,
+				tags: req.body.tags
+			});
+			
+			res.json({
+				success: true
+			});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: "Fault to update the post."
 		})
 	}
 }
